6b936d563d954026ee8590cc1faf9e96b5a6bf9b
Anurag Thakur <anurag.thakur@wingify.com>
2020-09-08 05:47:27 +0530

    nodeExtract || download file

--- routes/extract.js
+++ routes/extract.js
@@ -2,57 +2,121 @@ var express = require('express');
 var cors =require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
+var MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+
+
+var mongo_url = "mongodb+srv://anurag-cloud_mongodb_com:Qt6nwm5PUzrFK6m@cluster0.t9r2z.mongodb.net:27017?retryWrites=true&w=majority";
+var dbName="flipkart";
 //const htmlParse = require('node-html-parser');
+//node --max-old-space-size=4096 ./bin/www start
 
 var router =express.Router();
 const app = express();
 app.use(cors({origin: '*'}));
+
+console.log("version=1.2")
  
-var links={links:[]};
-let index=0;
+var links={links:[],index:[]};
+let index=0,iterate=500;
+
+category=(link)=>{
+    return new Promise(function(resolve, reject){
+    if (link.indexOf('/pr?')>5){
+        cat='Category Page';
+        resolve({category: cat, link: link});
+        
+    }else if(link.indexOf('/p/')>5){
+        cat='Product Page';
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('cart')>5){
+        cat='Cart Page';
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('login')>5){
+        cat='Login Page';
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('search')>5){
+        cat='Search Page';
+        resolve({category: cat, link: link});
+
+    }else if(link== "https://flipkart.com/" || link== "https://www.flipkart.com/" 
+    || link== "https://www.flipkart.com" || link== "https://flipkart.com"){
+        cat='Home Page'
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('offer')>5){
+        cat='Offer Page'
+        resolve({category: cat, link: link});
+
+    }else{
+        cat='Other Pages'
+        resolve({category: cat, link: link});
+
+    }
+    })
+};
+
+
+getPage=(url)=>{
+    return new Promise(function(resolve, reject){
+        axios.get(url, { headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' }  })
+        .then(response=>{
+            resolve(response);
+        })
+        .catch(error=> {
+            reject(error);
+        });
+    });
+};
+
+getAllLinks=(url)=>{
+    return new Promise(function(resolve, reject){
+    getPage(url).then(response=>{
+            const $ = cheerio.load(response.data);
+            $("a").each(function(i, elem) {
+                ($(this).attr("href") && $(this).attr("href").indexOf('http')!=0)? data="https://flipkart.com"+$(this).attr("href"): data=$(this).attr("href");
+            if(links.index.indexOf(data) === -1) {
+                links.index.push(data);
+                    category(data)
+                    .then((result)=>{
+                        fs.appendFileSync('message.txt', JSON.stringify(result)+',');
+                    })
+                    .catch(err => {
+                        console.log("error while getting categ");  
+                    })
+                }
+            });
+            if(index <= iterate){
+                console.log(index);
+                console.log(links.index[index]);
+                getAllLinks(links.index[index]).then(function(value){resolve(value)});
+                index++;
+            }
+            else{
+                resolve(links.index[index]);
+            }
+        }).catch(err => {
+            // what now?
+            console.log(err);
+            if(index <= iterate){
+                console.log(index);
+                console.log(links.index[index]);
+                getAllLinks(links.index[index]).then(function(value){resolve(value)});
+                index++;
+            }  
+        })
+    })
+    };
+
 
 
 router.get("/",function(req,res,next) {
-    axios.get(req.query.url, { headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' }  })
-    .then(response=>{
+    getPage(req.query.url).then(response=>{
         const $ = cheerio.load(response.data);
         console.log(req.query.url);
-
-        getUrls=(url)=>{
-            axios.get(url, { headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' }  })
-            .then(response=>{
-                const $ = cheerio.load(response.data);
-                $("a").each(function(i, elem) {
-                    ($(this).attr("href") && $(this).attr("href").indexOf('http')!=0)? data="https://flipkart.com"+$(this).attr("href"): data=$(this).attr("href");
-                if(links.links.indexOf(data) === -1) {
-                        links.links.push(data);
-                    }
-                });
-                if(index <= 50){
-                    console.log(index);
-                    console.log(links.links[index]);
-                    getUrls(links.links[index]);
-                    index++;
-                }
-                else{
-                    res.setHeader('Access-Control-Allow-Origin', '*');
-                    res.send(links);
-                }
-            }).catch(err => {
-                // what now?
-                console.log(err);
-                if(index <= 50){
-                    console.log(index);
-                    console.log(links.links[index]);
-                    getUrls(links.links[index]);
-                    index++;
-                }
-                else{
-                    res.setHeader('Access-Control-Allow-Origin', '*');
-                    res.send(links);
-                }  
-            })
-        };
 
         if((req.query.url.indexOf('/pr?'))>1){
             var elements={"elements":[]};
@@ -114,16 +178,35 @@ router.get("/",function(req,res,next) {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.send(elements);
         }
-        else if(req.query.url== "https://flipkart.com/" || req.query.url== "https://www.flipkart.com/" || req.query.url== "https://www.flipkart.com" || req.query.url== "https://flipkart.com"){
+        else if( req.query.url== "https://www.flipkart.com/" || req.query.url== "https://www.flipkart.com" || req.query.url== "https://flipkart.com"){
             
-            let links=getUrls(req.query.url);
-        }
+            if(index<=iterate){
+                getAllLinks(req.query.url).then(()=>{
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.send(links);
+                })
+            }else{
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.send(links);
+            }
+        }else if(req.query.url== "https://flipkart.com/"){
+                fs.readFile('message.txt', (err, data) => { 
+                    console.log(data);
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.send(data);
+                 })
+            }
         else {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.send("not a pr/p/home  page");
         }
     })
-    .catch(error=> console.log(error));
+    .catch(error=> {
+        console.log(error)
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send("Incorrect URL");
+    });
+        
     });
     
     // res.setHeader('Access-Control-Allow-Methods', 'GET'); // If needed
--- routes/extractPage.js
+++ routes/extractPage.js
@@ -2,7 +2,14 @@ var express = require('express');
 var cors =require('cors');
 const axios = require('axios');
 const cheerio = require('cheerio');
+var MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+
+
+var mongo_url = "mongodb+srv://anurag-cloud_mongodb_com:Qt6nwm5PUzrFK6m@cluster0.t9r2z.mongodb.net:27017?retryWrites=true&w=majority";
+var dbName="flipkart";
 //const htmlParse = require('node-html-parser');
+//node --max-old-space-size=4096 ./bin/www start
 
 var router =express.Router();
 const app = express();
@@ -10,8 +17,48 @@ app.use(cors({origin: '*'}));
 
 console.log("version=1.2")
  
-var links={links:[]};
-let index=0,iterate=50;
+var links={links:[],index:[]};
+let index=0,iterate=500;
+
+category=(link)=>{
+    return new Promise(function(resolve, reject){
+    if (link.indexOf('/pr?')>5){
+        cat='Category Page';
+        resolve({category: cat, link: link});
+        
+    }else if(link.indexOf('/p/')>5){
+        cat='Product Page';
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('cart')>5){
+        cat='Cart Page';
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('login')>5){
+        cat='Login Page';
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('search')>5){
+        cat='Search Page';
+        resolve({category: cat, link: link});
+
+    }else if(link== "https://flipkart.com/" || link== "https://www.flipkart.com/" 
+    || link== "https://www.flipkart.com" || link== "https://flipkart.com"){
+        cat='Home Page'
+        resolve({category: cat, link: link});
+
+    }else if(link.indexOf('offer')>5){
+        cat='Offer Page'
+        resolve({category: cat, link: link});
+
+    }else{
+        cat='Other Pages'
+        resolve({category: cat, link: link});
+
+    }
+    })
+};
+
 
 getPage=(url)=>{
     return new Promise(function(resolve, reject){
@@ -31,26 +78,33 @@ getAllLinks=(url)=>{
             const $ = cheerio.load(response.data);
             $("a").each(function(i, elem) {
                 ($(this).attr("href") && $(this).attr("href").indexOf('http')!=0)? data="https://flipkart.com"+$(this).attr("href"): data=$(this).attr("href");
-            if(links.links.indexOf(data) === -1) {
-                    links.links.push(data);
+            if(links.index.indexOf(data) === -1) {
+                links.index.push(data);
+                    category(data)
+                    .then((result)=>{
+                        fs.appendFileSync('message.txt', JSON.stringify(result)+',');
+                    })
+                    .catch(err => {
+                        console.log("error while getting categ");  
+                    })
                 }
             });
             if(index <= iterate){
                 console.log(index);
-                console.log(links.links[index]);
-                getAllLinks(links.links[index]).then(function(value){resolve(value)});
+                console.log(links.index[index]);
+                getAllLinks(links.index[index]).then(function(value){resolve(value)});
                 index++;
             }
             else{
-                resolve(links.links[index]);
+                resolve(links.index[index]);
             }
         }).catch(err => {
             // what now?
             console.log(err);
             if(index <= iterate){
                 console.log(index);
-                console.log(links.links[index]);
-                getAllLinks(links.links[index]).then(function(value){resolve(value)});
+                console.log(links.index[index]);
+                getAllLinks(links.index[index]).then(function(value){resolve(value)});
                 index++;
             }  
         })
@@ -124,7 +178,7 @@ router.get("/",function(req,res,next) {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.send(elements);
         }
-        else if(req.query.url== "https://flipkart.com/" || req.query.url== "https://www.flipkart.com/" || req.query.url== "https://www.flipkart.com" || req.query.url== "https://flipkart.com"){
+        else if( req.query.url== "https://www.flipkart.com/" || req.query.url== "https://www.flipkart.com" || req.query.url== "https://flipkart.com"){
             
             if(index<=iterate){
                 getAllLinks(req.query.url).then(()=>{
@@ -135,7 +189,13 @@ router.get("/",function(req,res,next) {
                 res.setHeader('Access-Control-Allow-Origin', '*');
                     res.send(links);
             }
-        }
+        }else if(req.query.url== "https://flipkart.com/"){
+                fs.readFile('message.txt', (err, data) => { 
+                    console.log(data);
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.send(data);
+                 })
+            }
         else {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.send("not a pr/p/home  page");
